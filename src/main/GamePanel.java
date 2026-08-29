40d4879d1d2791d5dfdc48768a76ffada755b323
priyojit singha <priyojitsingha01@gmail.com>
2026-08-29 16:15:21 +0530

    Initial commit

---
+++ src/main/GamePanel.java
@@ -0,0 +1,22 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+//    Screen settings
+    final int originalTileSize = 16; // default size of any npc, mopb or player size [16*16]
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * 3; // 48*48 actual tile size
+    final int maxScreenColumn = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenColumn; // 768 pixles
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight)); // sets the size of this class (JPanel)
+        this.setBackground(Color.GRAY);
+        this.setDoubleBuffered(true);
+    }
+}
