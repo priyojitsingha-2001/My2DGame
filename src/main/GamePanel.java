ad39014a115d8023b098e32f934b8ec63b9c5b64
priyojit singha <priyojitsingha01@gmail.com>
2026-08-29 16:17:20 +0530

    Creating the game window

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
 //    Screen settings
     final int originalTileSize = 16; // default size of any npc, mopb or player size [16*16]
     final int scale = 3;
@@ -14,9 +14,21 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenColumn; // 768 pixles
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    Thread gameThread;
+
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight)); // sets the size of this class (JPanel)
-        this.setBackground(Color.GRAY);
+        this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,20 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2d Adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack(); // Causes this window to be sized to fit the preffered size and layouts of its subcomponents (=GamePanel)
+
+        window.setLocationRelativeTo(null); //Not specify the loactio of the window = the window will be displayed at the center of the screen
+        window.setVisible(true);
+    }
 }
