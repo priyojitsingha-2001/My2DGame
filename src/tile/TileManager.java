06840964b61415dc2154eb145f185fcdbe141546
priyojit singha <priyojitsinghafirstone@gmail.com>
2026-09-15 22:41:00 +0530

    implementing logic to render a small map for our game using the matrix technique

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,16 +4,22 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileManager {
     GamePanel gamePanel;
     Tile[] tiles;
+    int mapTileNum [][];
 
     public TileManager(GamePanel gamePanel){
         this.gamePanel = gamePanel;
         tiles = new Tile[10]; // we are going to have 10 types of
+        mapTileNum = new int[gamePanel.maxScreenColumn][gamePanel.maxScreenRow];
         getTileImage();
+        loadMap("/maps/map01.txt");
     }
 
     public void getTileImage(){
@@ -33,13 +39,46 @@ public class TileManager {
         }
     }
 
+    public void loadMap(String filePath){
+        try {
+            InputStream is = getClass().getResourceAsStream(filePath);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            int col = 0;
+            int row = 0;
+
+            while(col < gamePanel.maxScreenColumn && row < gamePanel.maxScreenRow){
+
+                String line = br.readLine();
+
+                while(col < gamePanel.maxScreenColumn){
+                    String numbers[] = line.split(" ");
+                    int num = Integer.parseInt(numbers[col]);
+                    mapTileNum[col][row] = num;
+                    col ++;
+                }
+
+                if(col == gamePanel.maxScreenColumn){
+                    col = 0;
+                    row++;
+                }
+            }
+            br.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void draw(Graphics2D graphics2D){
         int col = 0;
         int row = 0;
         int x = 0;
         int y = 0;
         while(col < gamePanel.maxScreenColumn && row < gamePanel.maxScreenRow){
-            graphics2D.drawImage(tiles[0].image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+
+            int tileNumber = mapTileNum[col][row];
+            graphics2D.drawImage(tiles[tileNumber].image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
             col ++;
             x += gamePanel.tileSize;
 
